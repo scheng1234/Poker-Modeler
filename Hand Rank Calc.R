eace2b8dd4526730c0527064030be94f38adc2be
scheng1234 <scheng1234@gmail.com>
2022-10-16 22:28:21 -0600

    Updates to hand rank calc and additional content

--- Hand Rank Calc.R
+++ Hand Rank Calc.R
@@ -9,7 +9,6 @@
 # ===========================
 
 source("Libraries.R")
-source("Hand Calc Support.Rmd")
 
 
 # Hand Data Input ---------------------------------------------------------
@@ -24,7 +23,7 @@ source("Hand Calc Support.Rmd")
 
 # Created by starting with a 2 card hand and adding cards from flop (3), river(1) and turn (1)
 
-Hand <- tibble(Card = c("2S", "5S", "3S" , "4S", "AS" , "QH", "6H")) %>% 
+Hand <- tibble(Card = c("AS", "QS", "8S" , "JS", "10S" , "9S", "8H")) %>% 
   left_join(., Deck)
 
 
@@ -34,8 +33,12 @@ Hand <- tibble(Card = c("2S", "5S", "3S" , "4S", "AS" , "QH", "6H")) %>%
 
 
 
-Straight <- function(){}
-Flush <- function(){}
+Straight <- function(){x=5}
+Flush <- function(g){ 
+  testfunc <- function(y){x=6
+  return(x)
+  }
+}
 
 
 
@@ -45,14 +48,33 @@ Flush <- function(){}
 
 # Royal flush : {AH, KH, QH, JH, 10H}
 
-test <- table(Hand$Suit)
-test >= 5
-
-Hand$Value
+# CHeck Flush, then straight, then high card == A
 
 # Straight flush
+
+# Check Flush, then Straight
+
+
+
 # Four of a kind
+temp <- Hand
+test <- c(table(temp$Value) == 4)
+reduce(test, `|`) # This statement must be true
+
+temp$A <- test[match(temp$Value, names(test))]
+temp[temp$A, 1:4]
+
 # Full house
+temp <- Hand
+test1 <- c(table(temp$Value) == 3)
+test2 <- c(table(temp$Value) == 2)
+
+reduce(test1, `|`) && reduce(test2, `|`) #This statement must be true or it is not FH
+
+temp$A <- test1[match(temp$Value, names(test))]
+temp$B <- test2[match(temp$Value, names(test))]
+
+temp[temp$A | temp$B, 1:4]
 
 
 # Flush
@@ -63,19 +85,34 @@ reduce(test, or) #Must Pass
 temp$A <- test[match(temp$Suit, names(test))]
 temp[temp$A, 1:4] #Return Flush
 
-### Straight
+### Straight ###
+
+# Unique Rule that A can act as a low card as well. The if statement appends low values Aces to the hand. We also need to select for the highest straight.
+
 
 temp <- Hand
+if("A" %in% temp$Number){
+  temp <- add_row(temp, slice_max(temp, Value)[,1:3], Value = 0)
+  }
+
 temp <- temp[order(temp$Value),]
 
-temp$Value - temp$Value
+test <- rle(temp$Value - lag(temp$Value)) # counts run lengths
+reduce(test$lengths >= 4 & test$values == 1,`|`) #grabbing values of 1 (difference between n_2 and n_1) & 4 or more means that we have a straight
 
+with(rle(temp$Value - lag(temp$Value)), rep(lengths, lengths)) # This somehow works but not sure why
 
+temp$A <- rep(test$lengths, test$lengths)
+temp$B <- with(rle(temp$Value - lead(temp$Value)), rep(lengths, lengths))
+
+slice_tail(temp[temp$A >= 4 | temp$B >= 4,1:4],n = 5)
+
+# Wow this was hard...
 
 # Three of a kind
 temp <- Hand
 test <- c(table(temp$Value) >= 3)
-reduce(test, or) #Must Pass
+reduce(test, `|`) #Must Pass
 
 temp$A <- test[match(temp$Value, names(test))]
 temp[temp$A, 1:4] #Return Three of a kind
@@ -86,7 +123,7 @@ temp[temp$A, 1:4] #Return Three of a kind
 temp <- Hand
 test <- c(table(temp$Value) >= 2)
 
-reduce(test, or) #Must Pass test
+reduce(test, `|`) #Must Pass test
 sum(test) == 2 #Must pass count
 
 temp$A <- test[match(temp$Value, names(test))]
@@ -96,7 +133,7 @@ temp[temp$A, 1:4] #Return Two Pair
 temp <- Hand
 test <- c(table(temp$Value) >= 2)
 
-reduce(test, or) #Must Pass test
+reduce(test, `|`) #Must Pass test
 sum(test) == 1 #Must Pass count
 
 temp$A <- test[match(temp$Value, names(test))]
